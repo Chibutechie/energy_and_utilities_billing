3fcabf4945fd5d915e71616d8021e9dbce17c52c
Chibutechie <chibuezeanalyst@gmail.com>
2025-10-21 09:04:15 +0100

    Modified pandas file

--- extractor/extract.py
+++ extractor/extract.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 # extract csv file using url
-url = "https://huggingface.co/datasets/electricsheepafrica/nigerian_energy_and_utilities_billing_payments/resolve/main/nigerian_energy_and_utilities_billing_payments.parquet"
 
-data = pd.read_parquet(url)
+data = pd.read_parquet("https://huggingface.co/datasets/electricsheepafrica/nigerian_energy_and_utilities_billing_payments/resolve/main/nigerian_energy_and_utilities_billing_payments.parquet")
 
 print(data.head(10))
 print(data.columns)
